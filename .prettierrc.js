be201c3d5ca48553b4ceafbbe8da9d108998775b
Alexander Sosov <a.sosov@vlig44.ru>
2024-05-14 10:53:01 +0300

    добавить стартовые файлы

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  indent_style: "space",
+  indent_size: 2,
+  max_line_length: 80,
+  jsxSingleQuote: false,
+  endOfLine: "auto"
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="fsfsfs">
+      Проект
+    </div>
+  )
+}
+
+export default App
